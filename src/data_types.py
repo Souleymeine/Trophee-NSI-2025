e62eb939fcc40f996accdd5484b7571483d0bdab
Souleymeine <souleymeine@gmail.com>
2025-03-04 16:35:14 +0000

    Ajoute la 'metclass' singleton
    Permet de créer des classes qu'une n'ont qu'une seule instance

--- src/data_types.py
+++ src/data_types.py
@@ -8,6 +8,14 @@ from dataclasses import dataclass
 import math
 from decimal import Decimal
 
+# https://stackoverflow.com/questions/6760685/what-is-the-best-way-of-implementing-singleton-in-python
+class Singleton(type):
+    """Les classes utilisant 'Singleton' comme 'metaclass' n'auront qu'une seule instance."""
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 @dataclass
 class RGB:
