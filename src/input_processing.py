b3c85f6d61e9f2b76f828c56a04010462ac2eb4c
Souleymeine <souleymeine@gmail.com>
2025-03-05 23:14:27 +0000

    Corrige un problème de compatibilité
    Ajout d'une condition pour définir les fonctions

--- src/input_processing.py
+++ src/input_processing.py
@@ -22,79 +22,81 @@ def on_mouse(info: mouse.Info):
     gohome()
     print("\x1b[0J", end="")
 
-def parse_xterm_mouse_tracking_sequence(sequence: list[bytes], last_click: mouse.Click | None) -> mouse.Info:
-    """Analyse la séquence de caractère pour l'interpréter en une classe de type mouse.Info"""
+if sys.platform != "win32":
+    def parse_xterm_mouse_tracking_sequence(sequence: list[bytes], last_click: mouse.Click | None) -> mouse.Info:
+        """Analyse la séquence de caractère pour l'interpréter en une classe de type mouse.Info"""
 
-    # Convertie les caractères en valeures numériques
-    data = [int.from_bytes(byte) - 32 for byte in sequence]
+        # Convertie les caractères en valeures numériques
+        data = [int.from_bytes(byte) - 32 for byte in sequence]
 
-    # Le code se réfère à ce format : https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Mouse-Tracking
+        # Le code se réfère à ce format : https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Mouse-Tracking
 
-    class _XtermMouseFlags(IntFlag):
-        SHIFT = 4
-        ALT = 8
-        CTRL = 16
-        MOVE = 32
+        class _XtermMouseFlags(IntFlag):
+            SHIFT = 4
+            ALT = 8
+            CTRL = 16
+            MOVE = 32
 
-    mouse_click = None
-    mouse_button = None
-    mouse_button_released = False
-    mouse_wheel = None
-    mouse_key_flags = 0
+        mouse_click = None
+        mouse_button = None
+        mouse_button_released = False
+        mouse_wheel = None
+        mouse_key_flags = 0
 
-    for flag in _XtermMouseFlags:
-        if data[3] & flag != 0:
-            mouse_key_flags += flag
+        for flag in _XtermMouseFlags:
+            if data[3] & flag != 0:
+                mouse_key_flags += flag
 
-    match data[3] - mouse_key_flags:
-        case 0:
-            mouse_button = mouse.Button.LEFT
-        case 1:
-            mouse_button = mouse.Button.MIDDLE
-        case 2:
-            mouse_button = mouse.Button.RIGHT
-        case 3: # Aucun click enfoncé
-            mouse_button_released = True
-            if last_click != None and not last_click.released:
-                mouse_button = last_click.button
-        case 64:
-            mouse_wheel = mouse.Wheel.SCROLL_UP
-        case 65:
-            mouse_wheel = mouse.Wheel.SCROLL_DOWN
+        match data[3] - mouse_key_flags:
+            case 0:
+                mouse_button = mouse.Button.LEFT
+            case 1:
+                mouse_button = mouse.Button.MIDDLE
+            case 2:
+                mouse_button = mouse.Button.RIGHT
+            case 3: # Aucun click enfoncé
+                mouse_button_released = True
+                if last_click != None and not last_click.released:
+                    mouse_button = last_click.button
+            case 64:
+                mouse_wheel = mouse.Wheel.SCROLL_UP
+            case 65:
+                mouse_wheel = mouse.Wheel.SCROLL_DOWN
 
-    if mouse_button != None:
-        mouse_click = mouse.Click(mouse_button, mouse_button_released)
-
-    return mouse.Info(mouse_click, mouse_wheel, Coord(data[-2], data[-1]), mouse_key_flags)
-
-def parse_windows_mouse_event(event: PyINPUT_RECORDType, last_click: mouse.Click | None) -> mouse.Info:
-    """Analyse l'évènement renvoyé par le terminal et le formatte en un objet de type 'mouse.Info'"""
-    mouse_click = None
-    mouse_button = None
-    mouse_button_released = False
-    mouse_wheel = None
-    mouse_key_flags = 0
-    
-    if event.EventFlags & win32con.MOUSE_WHEELED:
-        # Lorsque la molette est utilisée, la valeur avait l'air d'approcher les 2^32 et 2^31, un bitshift a fait l'affaire,
-        # aucune idée de pourquoi ni comment mais ça m'a l'air d'être l'usage attendu...
-        # Peut être ? https://learn.microsoft.com/fr-fr/windows/win32/api/winuser/nf-winuser-mouse_event
-        mouse_wheel = mouse.Wheel(event.ButtonState >> 32 - 1)
-    else:
-        if event.ButtonState & 1:
-            mouse_button = mouse.Button.LEFT
-        elif event.ButtonState & 2:
-            mouse_button = mouse.Button.RIGHT
-        elif event.ButtonState & 4:
-            mouse_button = mouse.Button.MIDDLE
-        elif event.ButtonState == 0 and (last_click != None and not last_click.released): # Aucun click enfoncé
-            mouse_button = last_click.button
-            mouse_button_released = True
-        
         if mouse_button != None:
             mouse_click = mouse.Click(mouse_button, mouse_button_released)
-    
-    return mouse.Info(mouse_click, mouse_wheel, Coord(event.MousePosition.X + 1, event.MousePosition.Y + 1), event.ControlKeyState)
+
+        return mouse.Info(mouse_click, mouse_wheel, Coord(data[-2], data[-1]), mouse_key_flags)
+
+if sys.platform == "win32":
+    def parse_windows_mouse_event(event: PyINPUT_RECORDType, last_click: mouse.Click | None) -> mouse.Info:
+        """Analyse l'évènement renvoyé par le terminal et le formatte en un objet de type 'mouse.Info'"""
+        mouse_click = None
+        mouse_button = None
+        mouse_button_released = False
+        mouse_wheel = None
+        mouse_key_flags = 0
+        
+        if event.EventFlags & win32con.MOUSE_WHEELED:
+            # Lorsque la molette est utilisée, la valeur avait l'air d'approcher les 2^32 et 2^31, un bitshift a fait l'affaire,
+            # aucune idée de pourquoi ni comment mais ça m'a l'air d'être l'usage attendu...
+            # Peut être ? https://learn.microsoft.com/fr-fr/windows/win32/api/winuser/nf-winuser-mouse_event
+            mouse_wheel = mouse.Wheel(event.ButtonState >> 32 - 1)
+        else:
+            if event.ButtonState & 1:
+                mouse_button = mouse.Button.LEFT
+            elif event.ButtonState & 2:
+                mouse_button = mouse.Button.RIGHT
+            elif event.ButtonState & 4:
+                mouse_button = mouse.Button.MIDDLE
+            elif event.ButtonState == 0 and (last_click != None and not last_click.released): # Aucun click enfoncé
+                mouse_button = last_click.button
+                mouse_button_released = True
+            
+            if mouse_button != None:
+                mouse_click = mouse.Click(mouse_button, mouse_button_released)
+        
+        return mouse.Info(mouse_click, mouse_wheel, Coord(event.MousePosition.X + 1, event.MousePosition.Y + 1), event.ControlKeyState)
 
 
 def listen_to_input():
