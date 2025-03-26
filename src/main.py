34f57addbd8aa90b4e8a9a307cad380c0d8f0e9a
Souleymeine <souleymeine@gmail.com>
2025-03-26 07:42:37 +0000

    Gère la méthode "spawn" pour les processus et ajoute un test
    Avec "fork", pas besoin d'utiliser 'input_process.join()'. Cependant,
    utiliser 'input_process.join()' revient au même, comme si la ligne était
    à la fin du programme avec "fork", ce qui est le cas de manière
    explicite ici.

--- src/main.py
+++ src/main.py
@@ -3,22 +3,26 @@
 # Projet : pyscape
 # Auteurs : Rabta Souleymeine
 
+import multiprocessing
 import sys
 import os
+from data_types import RGB, Coord
+from escape_sequences import print_bgcolor_at
 import input_processing
 import input_properties
 import terminal
+from input_properties import MouseInfo, MouseButton
 from terminal import Info, TerminalInfoManager, TerminalInfoProxy
-from multiprocessing import Process, Queue
+from multiprocessing import Lock, Process, Queue
 from threading import Thread
 from event_listeners import listeners
 if sys.platform != "win32":
     import signal
 
 def clean_exit():
-    input_process.kill()
     terminal.reset(shared_terminal_state)
     terminal_info_manager.shutdown()
+    input_process.kill()
 
     sys.exit(0)
 
@@ -46,6 +50,19 @@ def event_reciever():
                 for listener in listeners.resize_listeners:
                     listener(event_info)
 
+
+@listeners.on_mouse
+def paint(info: MouseInfo):
+    brush_size = 5
+
+    if info.click is not None and (info.click.button == MouseButton.LEFT or info.click.button == MouseButton.RIGHT):
+        for x in range(-(brush_size * 2) + 1, 2*brush_size):
+            for y in range(-brush_size + 1, brush_size):
+                if info.coord.x + x >= 1 and info.coord.y + y >= 1:
+                    color = RGB(0, int((info.coord.y/os.get_terminal_size().lines)*255), 0) if info.click.button == MouseButton.LEFT else RGB(0,0,0)
+                    print_bgcolor_at(color, Coord(info.coord.x + x, info.coord.y + y))
+
+
 if __name__ == "__main__":
     # NOTE : l'ordre des instructions suivantes est important !
     # Merci à ce post qui m'a permis de ne pas tomber dans la folie après plusieurs jours de recherches.
@@ -58,9 +75,11 @@ if __name__ == "__main__":
     terminal_info_manager = TerminalInfoManager()
     terminal_info_manager.register('Info', Info, TerminalInfoProxy)
     terminal_info_manager.start()
+    
 
     shared_terminal_state = terminal_info_manager.Info() # type: ignore
     event_queue = Queue()
+    lock = Lock()
     input_process = Process(target=input_processing.listen_to_input, args=(shared_terminal_state, event_queue), name="InputProcess")
 
     terminal.init(shared_terminal_state)
@@ -70,6 +89,5 @@ if __name__ == "__main__":
     event_listener_thread = Thread(target=event_reciever, daemon=True)
     event_listener_thread.start()
     
-@listeners.on_mouse
-def a(info):
-    print(f"Exemple : {info}", end="\r\n")
+    input_process.join()
+
