6251b949a558c31c1a89afc0eeb9cfa5762bf7ef
Souleymeine <souleymeine@gmail.com>
2025-03-27 15:47:00 +0000

    Restructure les classes du module tui

--- src/tui/box.py
+++ src/tui/box.py
@@ -1,10 +1,11 @@
 #Projet : pyscape
 #Auteurs : Rabta Souleymeine
 
+import warnings
 from typing import Final
 from escape_sequences import gohome, print_at, set_fgcolor, reset_fgcolor
-from data_types import RGB, Anchor, Coord
-from tui.TUI_element import TUIElement
+from data_types import RGB, Coord
+from tui.base import TUIElement, Positioning
 
 # Historiquement, ces caractères ont été inventé pour afficher des bordures des 
 # éléments d'interface directement dans le terminale, à la manière de l'ASCII art.
@@ -36,43 +37,13 @@ class Box(TUIElement):
         On peut également l'afficher avec des caractères spéciaux prévus à cet effet, en choisissant sa couleur, en arrondissant les coins ou non.
         """
 
-    def __init__(self, position: Coord, anchor: Anchor, width: int, height: int,
-        visible: bool = True, color: RGB = RGB(255, 255, 255),
-        rounded: bool = False, show_anchor: bool = False, z_index: int = 0):
-        """
-            Initialise une boîte avec les propriétés spécifiées.
-
-            Args:
-                position: Position de la boîte
-                anchor: Point d'ancrage de la boîte
-                width: Largeur de la boîte
-                height: Hauteur de la boîte
-                visible: Si la boîte est visible
-                color: Couleur de la boîte
-                rounded: Si les coins sont arrondis
-                show_anchor: Afficher le point d'ancrage (débogage)
-                z_index: Indice Z pour l'ordre d'affichage
-        """
-        super().__init__(position, anchor, width, height, z_index)
-        self._visible = visible
+    def __init__(self, positioning: Positioning, rounded: bool = False, color = RGB(255, 255, 255), show_anchor: bool = False,
+                 z_index: int = 0, visible: bool = True):
         self._color = color
         self._rounded = rounded
         self._show_anchor = show_anchor
 
-        self._validate_properties()
-
-    def _validate_properties(self):
-        """Valide les propriétés et émet des avertissements si nécessaire."""
-        import warnings
-
-        if self._color and not self._visible:
-            warnings.warn("Warning: La couleur est définie mais la boîte n'est pas visible (visible = False).")
-        if self._rounded and not self._visible:
-            warnings.warn("Warning: Les coins arrondis sont activés mais la boîte n'est pas visible (visible = False).")
-        if self._show_anchor and not self._visible:
-            warnings.warn(
-                "Warning: L'affichage du point d'ancrage est activé mais la boîte n'est pas visible (visible = False).")
-
+        super().__init__(positioning, z_index=z_index, visible=visible, bg_col=None, fg_col=color)
 
     def render(self) -> None:
         # Ici on déterminera pour chaque cas la position du coin supérieur gauche, puisqu'il est le plus proche 
@@ -80,58 +51,42 @@ class Box(TUIElement):
         # On pourra ensuite aller à droite ou en bas pour compléter la forme sans avoir à utiliser des boucles
         # pour remonter dans le repère avec des pas négatifs.
 
-        TOP_LEFT_COORD: Final[Coord] = self.determine_top_left_coord() 
+        TOP_LEFT_COORD: Final[Coord] = super().top_left_coord
 
         if self._visible:
-            set_fgcolor(self.color) # Définie la couleur d'arrière plan pour tous les prochains caractères imprimés.
+            set_fgcolor(self._color) # Définie la couleur de text de tous les prochains caractères imprimés.
             
-            self.draw_rows(TOP_LEFT_COORD)
-            self.draw_columns(TOP_LEFT_COORD)
-            self.draw_corners(TOP_LEFT_COORD)
+            self.print_rows(TOP_LEFT_COORD)
+            self.print_columns(TOP_LEFT_COORD)
+            self.print_corners(TOP_LEFT_COORD)
 
             # Pour débugage ou démonstration
             if (self._show_anchor):
                 gohome()
-                print_at('+', Coord(self.position.x, self.position.y))
+                print_at('+', Coord(self._positioning.origin.x, self._positioning.origin.y))
             
-            reset_fgcolor() # Rétablie la couleur d'arrière plan par défaut
+            reset_fgcolor() # Rétablie la couleur du text par défaut
 
-    def determine_top_left_position(self) -> Coord:
-        match self.anchor:
-            case Anchor.CENTER:       return Coord(int(self.position.x - (self.width / 2)), int(self.position.y - (self.height / 2)))
-            case Anchor.TOP_LEFT:     return Coord(self.position.x, self.position.y)
-            case Anchor.TOP_RIGHT:    return Coord(self.position.x - self.width, self.position.y)
-            case Anchor.BOTTOM_LEFT:  return Coord(self.position.x, self.position.y - self.height)
-            case Anchor.BOTTOM_RIGHT: return Coord(self.position.x - self.width, self.position.y - self.height)
-    
-    def draw_rows(self, top_left_position: Coord):
+    def print_rows(self, top_left_position: Coord):
         # Leur taille est égale à la largeur du bouton - 2, l'espace manquant est occupé par un coin.
         # On note également qu'on se décale d'une cellule pour se faire, laissant donc les deux cellules dédiés aux coins intactes.
-        print_at(FULL_HORIZONTAL_BAR * (self.width - 2), Coord(top_left_position.x + 1, top_left_position.y))
-        print_at(FULL_HORIZONTAL_BAR * (self.width - 2), Coord(top_left_position.x + 1, top_left_position.y + self.height - 1))
+        print_at(FULL_HORIZONTAL_BAR * (self._positioning.width - 2), Coord(top_left_position.x + 1, top_left_position.y))
+        print_at(FULL_HORIZONTAL_BAR * (self._positioning.width - 2), Coord(top_left_position.x + 1, top_left_position.y + self._positioning.height - 1))
 
-    def draw_columns(self, top_left_position: Coord):
+    def print_columns(self, top_left_position: Coord):
         # Puisqu'on ne peut imprimer les colonnes en un seul appelle de 'print', on va descendre, à chaque itération, d'une cellule.
         # Comme pour les lignes, on laisse deux cellules intactes pour les coins, une en haut et une en bas.
         
         # Gauche
-        for current_cell_ordinate in range(1, self.height):
+        for current_cell_ordinate in range(1, self._positioning.height):
             print_at(FULL_VERTICAL_BAR, Coord(top_left_position.x, top_left_position.y + current_cell_ordinate))
         # Droite
-        for current_cell_ordinate in range(1, self.height):
-            print_at(FULL_VERTICAL_BAR, Coord(top_left_position.x + self.width - 1, top_left_position.y + current_cell_ordinate))
+        for current_cell_ordinate in range(1, self._positioning.height):
+            print_at(FULL_VERTICAL_BAR, Coord(top_left_position.x + self._positioning.width - 1, top_left_position.y + current_cell_ordinate))
 
-    def draw_corners(self, top_left_position: Coord):
+    def print_corners(self, top_left_position: Coord):
         print_at(TOP_LEFT_ROUND_CORNER if self._rounded else TOP_LEFT_CORNER,         top_left_position)
-        print_at(BOTTOM_LEFT_ROUND_CORNER if self._rounded else BOTTOM_LEFT_CORNER,   Coord(top_left_position.x, top_left_position.y + self.height - 1))
-        print_at(TOP_RIGHT_ROUND_CORNER if self._rounded else TOP_RIGHT_CORNER,       Coord(top_left_position.x + self.width - 1, top_left_position.y))
-        print_at(BOTTOM_RIGHT_ROUND_CORNER if self._rounded else BOTTOM_RIGHT_CORNER, Coord(top_left_position.x + self.width - 1, top_left_position.y + self.height - 1))
+        print_at(BOTTOM_LEFT_ROUND_CORNER if self._rounded else BOTTOM_LEFT_CORNER,   Coord(top_left_position.x, top_left_position.y + self._positioning.height - 1))
+        print_at(TOP_RIGHT_ROUND_CORNER if self._rounded else TOP_RIGHT_CORNER,       Coord(top_left_position.x + self._positioning.width - 1, top_left_position.y))
+        print_at(BOTTOM_RIGHT_ROUND_CORNER if self._rounded else BOTTOM_RIGHT_CORNER, Coord(top_left_position.x + self._positioning.width - 1, top_left_position.y + self._positioning.height - 1))
 
-    def on_mouse_hover(self):
-        return super().on_mouse_hover()
-    def on_render(self):
-        return super().on_render()
-    def on_mouse_click(self):
-        return super().on_mouse_click()
-    def on_mouse_exit(self):
-        return super().on_mouse_exit()
--- src/tui/text_area.py
+++ src/tui/text_area.py
@@ -2,65 +2,47 @@
 #Auteurs : Rabta Souleymeine
 
 from typing import Final
-from tui.TUI_element import TUIElement
+from input_properties import MouseInfo
+from tui.base import Positioning, ClickableElement, ColorsOnMouse
 from tui.box import Box
-from data_types import RGB, Alignment, Anchor, HorizontalAlignment, Coord, VerticalAlignment
+from data_types import Align, HorizAlign, Coord, VirtAlign
 from escape_sequences import ANSI_Styles, cat_goto, print_styled_at, print_styled
-from input_properties import KeyInfo, MouseInfo
 from utils import split_preserve
-from event_listeners import listeners, TUIElementMouseCallbacks
 
-class TextArea(TUIElement):
+class TextArea(ClickableElement):
     """Permet de représenter des zones de texte contenue dans un certain cadre, visible ou non."""
-    def __init__(self, position: Coord, anchor: Anchor, width: int, height: int, text: str, style: ANSI_Styles, alignment: Alignment, border_color = RGB(255, 255, 255), z_index = 0):
-        """
-        Initialise une zone de texte.
+    def __init__(self, positioning: Positioning, text: str, style: ANSI_Styles, colors_on_mouse: ColorsOnMouse, rounded=False, alignment = Align(HorizAlign.LEFT, VirtAlign.TOP),
+                 z_index: int = 0, visible: bool = True):
+        self._text = text
+        self._style = style
+        self._colors_on_mouse = colors_on_mouse
+        self._alignment = alignment
+        self._box = Box(positioning, visible=visible, rounded=rounded)
+        super().__init__(positioning, colors_on_mouse, z_index, visible)
 
-        Args:
-            text (str): Le texte à afficher
-            style (ANSI_Styles): Le style à appliquer au texte
-            alignment (Alignment): L'alignement horizontal et vertical du texte
-            box (Box): La boîte contenant le texte
-        """
-        super().__init__(position, anchor, width, height, z_index, True, 1)
-        self._box = Box(position, anchor, width, height, color=border_color)
-        self.text = text
-        self.style = style
-        self.alignment = alignment
-        self.border_color = border_color
-        listeners.register_mouse_callbacks(self, TUIElementMouseCallbacks(self._mouse_hover, self._mouse_exit, self._mouse_click))
-        listeners.register_key(self, self._type)
-        self.render()
+    def _on_hover(self, info: MouseInfo):
+        if self._colors_on_mouse.hover_fg_col is not None:
+            self._box._color = self._colors_on_mouse.hover_fg_col
 
-    def _mouse_hover(self, info: MouseInfo):
-        self.is_mouse_over = True
-        self._box.color = RGB(int(self.border_color.r/1.25), int(self.border_color.g/1.25), int(self.border_color.b/1.25))
         self.render()
-        self.on_mouse_hover()
-    def _mouse_click(self, info: MouseInfo):
+        super()._on_hover(info)
+    
+    def _on_click(self, info: MouseInfo):
         assert info.click is not None
         if info.click.released == False:
-            self._box.color = RGB(self.border_color.r//2, self.border_color.g//2, self.border_color.b//2)
-            self.render()
-            self.is_selected = True
-        elif info.click.released:
-            self._box.color = RGB(int(self.border_color.r/1.25), int(self.border_color.g/1.25), int(self.border_color.b/1.25))
-            self.render()
-        self.on_mouse_click()
-    def _mouse_exit(self, info: MouseInfo):
-        self._box.color = self.border_color
+            if self._colors_on_mouse.click_fg_col is not None:
+                self._box._color = self._colors_on_mouse.click_fg_col
+        else:
+            if self._colors_on_mouse.hover_fg_col is not None:
+                self._box._color = self._colors_on_mouse.hover_fg_col
         self.render()
-        self.on_mouse_exit()
-
-    def _type(self, info: KeyInfo):
-        pass
-
-    @property
-    def is_selected(self):
-        return self._is_selected
-    @is_selected.setter
-    def is_selected(self, value: bool):
-        self._is_selected = value
+        super()._on_click(info)
+    
+    def _on_mouse_exit(self, info: MouseInfo):
+        if self._colors_on_mouse.default_fg_col is not None:
+            self._box._color = self._colors_on_mouse.default_fg_col
+        self.render()
+        super()._on_mouse_exit(info)
 
     def wrapped_text(self, first_char_pos: Coord) -> str:
         """
@@ -72,7 +54,7 @@ class TextArea(TUIElement):
         Returns:
             str: Texte enveloppé avec les sauts de ligne et les positions appropriés
         """
-        MAX_LENGTH = self._box.width - 2
+        MAX_LENGTH = self._box.positioning.width - 2
 
         wrapped_text = cat_goto(first_char_pos)
         current_line_length = 0
@@ -91,7 +73,7 @@ class TextArea(TUIElement):
             wrapped_text += text
             current_line_length += len(text)
 
-        for raw_line in split_preserve('\n', self.text):
+        for raw_line in split_preserve('\n', self._text):
             if raw_line == "\n":
                 _concat_offset_linebreak()
             elif len(raw_line) < MAX_LENGTH:
@@ -116,17 +98,18 @@ class TextArea(TUIElement):
         """
         Dessine la boîte et le texte selon les paramètres spécifiés.
         """
-        self._box.render()
+        if self._visible:
+            self._box.render()
 
-        BOX_TOP_LEFT_COORD: Final[Coord] = self._box.determine_top_left_position()
-        FIRST_CHAR_COORD: Final[Coord] = self.determine_first_char_coord(BOX_TOP_LEFT_COORD)
+            BOX_TOP_LEFT_COORD: Final[Coord] = super().top_left_coord
+            FIRST_CHAR_COORD: Final[Coord] = self.determine_first_char_coord(BOX_TOP_LEFT_COORD)
 
-        is_text_inline: bool = len(self.text) <= self._box.width - 2
-        if is_text_inline:
-            print_styled_at(self.text, self.style, FIRST_CHAR_COORD)
-        else:
-            margin_pos = Coord(BOX_TOP_LEFT_COORD.x + 1, BOX_TOP_LEFT_COORD.y + 1)
-            print_styled(self.wrapped_text(margin_pos), self.style)
+            is_text_inline: bool = len(self._text) <= self._positioning.width - 2
+            if is_text_inline:
+                print_styled_at(self._text, self._style, FIRST_CHAR_COORD)
+            else:
+                margin_pos = Coord(BOX_TOP_LEFT_COORD.x + 1, BOX_TOP_LEFT_COORD.y + 1)
+                print_styled(self.wrapped_text(margin_pos), self._style)
 
     def determine_first_char_coord(self, box_top_left_coord: Coord) -> Coord:
         """
@@ -139,29 +122,22 @@ class TextArea(TUIElement):
             Coord: Coordonnées du premier caractère à imprimer
         """
         first_char_coord = Coord(0, 0)
-        match self.alignment.horizontal:
-            case HorizontalAlignment.LEFT:   
+        match self._alignment.horizontal:
+            case HorizAlign.LEFT:   
                 first_char_coord.x = box_top_left_coord.x + 1
-            case HorizontalAlignment.CENTER: 
-                first_char_coord.x = int(box_top_left_coord.x + self._box.width / 2 - len(self.text)/2)
-            case HorizontalAlignment.RIGHT:  
-                first_char_coord.x = box_top_left_coord.x + self._box.width - len(self.text) - 1
+            case HorizAlign.CENTER: 
+                first_char_coord.x = int(box_top_left_coord.x + self._positioning.width / 2 - len(self._text)/2)
+            case HorizAlign.RIGHT:  
+                first_char_coord.x = box_top_left_coord.x + self._positioning.width - len(self._text) - 1
         
-        match self.alignment.vertical:
-            case VerticalAlignment.TOP:    
+        match self._alignment.vertical:
+            case VirtAlign.TOP:    
                 first_char_coord.y = box_top_left_coord.y + 1
-            case VerticalAlignment.MIDDLE: 
-                first_char_coord.y = int(box_top_left_coord.y + self._box.height / 2)
-            case VerticalAlignment.BOTTOM: 
-                first_char_coord.y = box_top_left_coord.y + self._box.height - 2
+            case VirtAlign.MIDDLE: 
+                first_char_coord.y = int(box_top_left_coord.y + self._positioning.height / 2)
+            case VirtAlign.BOTTOM: 
+                first_char_coord.y = box_top_left_coord.y + self._positioning.height - 2
         
         return first_char_coord
 
-    def on_render(self):
-        return super().on_render()
-    def on_mouse_hover(self):
-        return super().on_mouse_hover()
-    def on_mouse_click(self):
-        return super().on_mouse_click()
-    def on_mouse_exit(self):
-        return super().on_mouse_exit()
+
