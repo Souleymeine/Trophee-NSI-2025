f17425ba572c4b070182970c62f9f23b0a007ef4
Souleymeine <souleymeine@gmail.com>
2025-03-27 15:50:44 +0000

    Enregistre les callback et gère les évènements

--- src/event_listeners.py
+++ src/event_listeners.py
@@ -1,32 +1,30 @@
-# from multiprocessing.managers import BaseManager, BaseProxy
 from dataclasses import dataclass
 from os import terminal_size
 from typing import Callable, Any
-
-from tui.TUI_element import TUIElement
 from data_types import EnsureSingle
 from input_properties import ArrowInfo, KeyInfo, MouseInfo
 
 @dataclass
-class TUIElementMouseCallbacks:
+class MouseCallbacks:
     on_hover: Callable[[MouseInfo], Any]
-    on_exit: Callable[[MouseInfo], Any]
     on_click: Callable[[MouseInfo], Any]
+    on_exit: Callable[[MouseInfo], Any]
 
 class EventListenerSubscriber(metaclass=EnsureSingle):
     def __init__(self) -> None:
-        self.mouse_listeners:  dict[TUIElement, TUIElementMouseCallbacks] = {}
-        self.key_listeners:    dict[TUIElement, Callable[[KeyInfo],           Any]] = {}
-        self.arrow_listeners:  dict[TUIElement, Callable[[ArrowInfo],         Any]] = {}
-        self.resize_listeners: dict[TUIElement, Callable[[terminal_size],     Any]] = {}
+        self.mouse_listeners:  dict[object, MouseCallbacks] = {}
+        self.key_listeners:    dict[object, Callable[[KeyInfo],           Any]] = {}
+        self.arrow_listeners:  dict[object, Callable[[ArrowInfo],         Any]] = {}
+        self.resize_listeners: dict[object, Callable[[terminal_size],     Any]] = {}
 
-    def register_mouse_callbacks(self, obj: TUIElement, callbacks: TUIElementMouseCallbacks):
+    def register_mouse_callbacks(self, obj: object, callbacks: MouseCallbacks):
         self.mouse_listeners[obj] = callbacks
-    def register_key(self, obj: TUIElement, callback: Callable[[KeyInfo], Any]):
+    def register_key(self, obj: object, callback: Callable[[KeyInfo], Any]):
         self.key_listeners[obj] = callback
-    def register_arrow(self, obj: TUIElement, callback: Callable[[ArrowInfo], Any]):
+    def register_arrow(self, obj: object, callback: Callable[[ArrowInfo], Any]):
         self.arrow_listeners[obj] = callback
-    def register_resize(self, obj: TUIElement, callback: Callable[[terminal_size], Any]):
+    def register_resize(self, obj: object, callback: Callable[[terminal_size], Any]):
         self.resize_listeners[obj] = callback
 
 listeners = EventListenerSubscriber()
+
--- src/event_managers.py
+++ src/event_managers.py
@@ -1,20 +1,23 @@
 import os
 from input_properties import ArrowInfo, KeyInfo, MouseInfo
 from event_listeners import EventListenerSubscriber
+from tui.base import ClickableElement
 
 
 def manage_mouse_event(event_info: MouseInfo, listeners: EventListenerSubscriber):
     # Gestion basique de la souris
-    for tui_element, mouse_callbacks in list(listeners.mouse_listeners.items()):
-        horizontal_align: bool = tui_element.position.x <= event_info.coord.x <= tui_element.position.x + tui_element.width
-        vertical_align: bool = tui_element.position.y <= event_info.coord.y <= tui_element.position.y + tui_element.height
+    for clickable_element, mouse_callbacks in list(listeners.mouse_listeners.items()):
+        assert type(clickable_element) == ClickableElement
+        top_left_coord = clickable_element.top_left_coord
+        horizontal_align: bool = top_left_coord.x <= event_info.coord.x <= top_left_coord.x + clickable_element.positioning.width
+        vertical_align: bool = top_left_coord.y <= event_info.coord.y <= top_left_coord.y + clickable_element.positioning.height
         if horizontal_align and vertical_align:
             if event_info.click is not None:
                 mouse_callbacks.on_click(event_info)
             else:
                 mouse_callbacks.on_hover(event_info)
-        elif tui_element.is_mouse_over == True:
-            tui_element.is_mouse_over = False
+        elif clickable_element.is_mouse_over == True:
+            clickable_element.is_mouse_over = False
             mouse_callbacks.on_exit(event_info)
 
 def manage_key_event(event_info: KeyInfo, listeners: EventListenerSubscriber):
